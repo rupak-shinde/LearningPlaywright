12f2ccb09ca57c9a676938acdc9a1f9d0b6f783c
rupak-shinde <hello.shinderrupak@gmail.com>
2026-02-13 06:23:03 +0530

    Initial commit

---
+++ chapter_01_Basics/03_verify_setup.js
@@ -0,0 +1,12 @@
+console.log(process.platform);
+
+// WIndows  Win32
+// MC  Darwin
+// Linux Linex
+
+console.log(process.arch);
+
+// x64
+//arm64
+
+console.log("Node Version:", process.version);
